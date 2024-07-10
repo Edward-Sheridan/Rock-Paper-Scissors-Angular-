04237db8ee6dfb3e1b2000c3edbc149ff8c29d43
Edward Fusire <fusire@sheridancollege.ca>
2024-07-10 11:05:18 -0400

    Initial commit (based on class material)

---
+++ src/app/game-service.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {Choice} from "./choice";
+import {GameData} from "./game-data";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  gameData: GameData;
+
+  constructor() {
+    this.gameData = new GameData(Choice.ROCK, Choice.ROCK);
+  }
+
+  private static choices: Choice[] = [Choice.PAPER, Choice.ROCK, Choice.SCISSORS]
+
+  private static getRandomChoice(): Choice {
+    return GameService.choices[Math.floor(3 * Math.random())]
+  }
+
+  reset(): void {
+    this.gameData = new GameData(Choice.ROCK, Choice.ROCK);
+  }
+
+  play(userChoice: Choice): void {
+    let computerChoice: Choice = GameService.getRandomChoice()
+    this.gameData = new GameData(userChoice, computerChoice);
+  }
+}
